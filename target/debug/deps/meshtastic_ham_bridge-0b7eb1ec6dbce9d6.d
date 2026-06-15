579e05ac5a9373196618240d164980135b810195
Dana Phillips <dphilli1_00@yahoo.com>
2026-06-15 14:35:38 -0400

    building the cargo.toml dependicy file

---
+++ target/debug/deps/meshtastic_ham_bridge-0b7eb1ec6dbce9d6.d
@@ -0,0 +1,5 @@
+C:\devspace\meshtastic-ham-bridge\target\debug\deps\meshtastic_ham_bridge-0b7eb1ec6dbce9d6.d: src\lib.rs
+
+C:\devspace\meshtastic-ham-bridge\target\debug\deps\libmeshtastic_ham_bridge-0b7eb1ec6dbce9d6.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/meshtastic_ham_bridge-8526c277e607d1ac.d
@@ -0,0 +1,5 @@
+C:\devspace\meshtastic-ham-bridge\target\debug\deps\meshtastic_ham_bridge-8526c277e607d1ac.d: src\lib.rs
+
+C:\devspace\meshtastic-ham-bridge\target\debug\deps\libmeshtastic_ham_bridge-8526c277e607d1ac.rmeta: src\lib.rs
+
+src\lib.rs:
